8ca2d561fd297e598ea7da585754d88f687f14bf
Bhola Yadav <153095725+Bhya23cse@users.noreply.github.com>
2025-02-23 22:04:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings  # Updated import
 from langchain_community.llms import CTransformers
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-from langchain_huggingface import HuggingFaceEmbeddings
-
 
 # Load the pdf files from the path
 loader = DirectoryLoader('data/', glob="*.pdf", loader_cls=PyPDFLoader)
